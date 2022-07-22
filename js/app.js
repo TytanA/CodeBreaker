976d2873bbc4477498e19834b36a52235ccb9015
TysonAnderson <tysonda621@gmail.com>
2022-07-22 14:56:22 -0700

    create event listens for the buttons

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //games constant variable
-const colorChoices = ['green', 'blue', 'red', 'yellow', 'gray', 'white', 'purple', 'orange']
+const colorChoices = ['green', 'blue', 'red', 'yellow', 'grey', 'white', 'purple', 'orange']
 
 //state variables
 let numberOfGuesses 
@@ -13,9 +13,51 @@ let guessCircleTwo;
 let guessCircleThree;
 let guesscircleFour;
 
+const board = [
+    null, null, null, null, [null,null,null,null],
+    null, null, null, null, [null,null,null,null],
+    null, null, null, null, [null,null,null,null],
+    null, null, null, null, [null,null,null,null],
+    null, null, null, null, [null,null,null,null],
+    null, null, null, null, [null,null,null,null],
+    null, null, null, null, [null,null,null,null],
+    null, null, null, null, [null,null,null,null],
+    null, null, null, null, [null,null,null,null],
+    null, null, null, null, [null,null,null,null],
+]
+
 let secretCode = ['', '', '', '',]
 
 //cashed elements
-const colorBtnEl = document.querySelectorAll('.colorButton');
-const submitBtnEl = document.querySelector('#submit');
-const deleteBtnEl = document.querySelector('#delete');
+const colorBtnEl = {
+    green: document.querySelector('#green'),
+    blue: document.querySelector('#blue'),
+    red: document.querySelector('#red'),
+    yellow: document.querySelector('#yellow'),
+    grey: document.querySelector('#grey'),
+    white: document.querySelector('#white'),
+    purple: document.querySelector('#purple'),
+    orange:document.querySelector('#orange')
+}
+const submitBtnEl = document.getElementById('submit');
+const deleteBtnEl = document.getElementById('delete');
+
+
+//set up event listeners for the buttons
+for (const color in colorBtnEl) {
+    colorBtnEl[color].addEventListener('click', addColor)}
+
+submitBtnEl.addEventListener('click', submit())
+deleteBtnEl.addEventListener('click', del())
+
+function addColor(e){
+    console.log(e.target.id)
+}
+
+function submit() {
+    console.log('submit')
+}
+
+function del() {
+    console.log('delete')
+}
